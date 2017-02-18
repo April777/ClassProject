569b36a8ee983678989076a404f943f2fe026e20
April <anpingqi7@gmail.com>
2017-02-18 22:16:00 +0000

    add customer files for test folder

--- src/main/java/edu/infsci2560/models/Customer.java
+++ src/main/java/edu/infsci2560/models/Customer.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package edu.infsci2560.models;
 
 import javax.persistence.Entity;
@@ -5,71 +10,66 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-/**
- * @author April
- */
 @Entity
-public class User {
-    
+public class Customer {
+
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
-    
-    protected User(){
-        
-    }
-    
-    public User(String firstName, String lastName){
+
+    protected Customer() {}
+
+    public Customer(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-    
+
     @Override
-    public String toString(){
-        return String.format("User[id=%d,firstName='%s',lastName='%s']",getId(), getFirstName(), getLastName());
+    public String toString() {
+        return String.format("Customer[id=%d, firstName='%s', lastName='%s']", getId(), getFirstName(), getLastName());
     }
-    
+
     /**
      * @return the id
      */
-    public Long getId(){
+    public Long getId() {
         return id;
     }
-    
+
     /**
      * @param id the id to set
      */
-    public void setId(Long id){
+    public void setId(Long id) {
         this.id = id;
     }
-    
+
     /**
      * @return the firstName
      */
-    public String getFirstName(){
+    public String getFirstName() {
         return firstName;
     }
-    
+
     /**
      * @param firstName the firstName to set
      */
-    public void setFirstName(String firstName){
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-    
+
     /**
      * @return the lastName
      */
-    public String getLastName(){
+    public String getLastName() {
         return lastName;
     }
-    
+
     /**
      * @param lastName the lastName to set
      */
-    public void setLastName(String lastName){
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
