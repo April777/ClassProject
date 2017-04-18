ce7ee40b921503560a1d1bac432022d76bc51771
April <anpingqi7@gamil.com>
2017-04-18 21:50:58 +0000

    modify

--- src/main/java/edu/infsci2560/repositories/CourseRepository.java
+++ src/main/java/edu/infsci2560/repositories/CourseRepository.java
@@ -1,9 +1,6 @@
 package edu.infsci2560.repositories;
 
 import edu.infsci2560.models.Course;
-import edu.infsci2560.models.CoursePk;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
